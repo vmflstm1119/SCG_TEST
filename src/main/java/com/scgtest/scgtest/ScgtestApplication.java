1ab3c4ca5620493d265225eb89f9300a3b7ee8df
vmflstm1119 <vmflstm1119@naver.com>
2022-06-02 17:24:35 +0900

    asd

---
+++ src/main/java/com/scgtest/scgtest/ScgtestApplication.java
@@ -0,0 +1,13 @@
+package com.scgtest.scgtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScgtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScgtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scgtest/scgtest/ScgtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scgtest.scgtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScgtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
